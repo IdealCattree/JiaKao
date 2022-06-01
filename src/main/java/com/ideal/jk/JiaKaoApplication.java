9a4d9bb9cbc92dc72ced03d25bd779c1ceab97d7
liuhaonan <liuhaonan@weidian.com>
2022-06-01 15:37:16 +0800

    bugfix

--- src/main/java/com/ideal/jk/JiaKaoApplication.java
+++ src/main/java/com/ideal/jk/JiaKaoApplication.java
@@ -1,6 +1,5 @@
 package com.ideal.jk;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
